b3bbeaf50f3824d6101f62b75bccf547201a622a
A08019DIRP_C2C.02.02 <A08019DIRP_C2C.02.02@accenture.com>
2019-04-16 15:08:13 +0530

    new change

---
+++ mavntest/src/main/java/mavntestpack/sampletest.java
@@ -0,0 +1,10 @@
+package mavntestpack;
+
+public class sampletest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TEst maven with Jenkins");
+	}
+
+}
